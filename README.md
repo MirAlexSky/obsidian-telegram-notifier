58376409cd6152f7356890c8935f9bb7e2156962
miralexsky <alexandr.sinus@gmail.com>
2024-03-03 02:22:26 +0400

    init(telegram-reminder): create project

--- README.md
+++ README.md
@@ -1,96 +1,26 @@
-# Obsidian Sample Plugin
+![obsidian](https://github.com/MirAlexSky/obsidian-telegram-notifier/images/obsidian.png "Obsidian")
+![telegram](https://github.com/MirAlexSky/obsidian-telegram-notifier/images/telegram.png "Telegram")
+# Telegram Notifier
+[![License](http://poser.pugx.org/miralexsky/ozon-logistics-api/license)](https://packagist.org/packages/miralexsky/ozon-logistics-api)
+### :white_check_mark: Plugin features
+- [X] Sending notification to telegram-bot by note property
+- [X] Sending notification to telegram-bot by inline mark
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+![screen-telegram](https://github.com/MirAlexSky/obsidian-telegram-notifier/images/screen3.png "Screen Telegram")
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+<h2>:rocket:Getting started</h2>
+First, you need to create your own telegram bot, it takes you about 3 minutes, 
+everything you need is write to @BotFather (https://t.me/BotFather), 
+create new bot via /newbot command and set it up as you like. You can find actual information 
+about creating telegram bot yourself if necessary.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+Next step, get your bot token and chat id. Fill them in plugin settings and fill others by your chose.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+That's all, reload plugin and enjoy!
 
-## First time developing plugins?
+To create a scheduled notification, add 'scheduled' property to the file (or 
+insert your own property, if you have changed it in settings). 
+Or you can add date right inside file body prepositioned with 📅 (you can change it in settings)
 
-Quick starting guide for new plugin devs:
-
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
-
-## Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+![screen](https://github.com/MirAlexSky/obsidian-telegram-notifier/images/screen1.png "Screen #1")
+![screen](https://github.com/MirAlexSky/obsidian-telegram-notifier/images/screen2.png "Screen #2")
--- main.ts
+++ main.ts
@@ -1,85 +1,181 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {App, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 interface MyPluginSettings {
-	mySetting: string;
+	telegramToken: string,
+	telegramChatId: string;
+	fileScheduleProperty: string,
+	fileSchedulePrefix: string,
+	notifyTime: string,
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	telegramToken: '',
+	telegramChatId: '',
+	fileScheduleProperty: 'scheduled',
+	fileSchedulePrefix: '📅',
+	notifyTime: '6:00',
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	filesToNotify: Object[] = [];
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		let pluginIsConfigured: boolean = Boolean(
+			this.settings.telegramToken
+				&& this.settings.telegramChatId
+				&& this.settings.fileScheduleProperty
+				&& this.settings.fileSchedulePrefix);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		if (pluginIsConfigured) {
+			this.init(this.settings.fileScheduleProperty, this.settings.fileSchedulePrefix);
+		} else {
+			new Notice('Please configure the plugin settings');
+		}
 	}
 
-	onunload() {
+	private init(fileScheduleProperty: string, fileSchedulePrefix: string) {
+		this.app.workspace.onLayoutReady(async () => {
+			this.setIntervalScan();
+		});
+	}
 
+	private setIntervalScan() {
+		let scanInProgress = true;
+
+		this.scan().finally(() => {
+			scanInProgress = false;
+		});
+
+		this.registerInterval(
+			window.setInterval(() => {
+				if (scanInProgress) {
+					return;
+				}
+				scanInProgress = true;
+
+				this.scan().finally(() => {
+					scanInProgress = false;
+				});
+			}, 50000)
+		);
+	}
+
+	private async scan(): Promise<void> {
+		const {vault} = this.app;
+
+		this.filesToNotify = [];
+
+		for (const file of vault.getMarkdownFiles()) {
+			let fileName = file.basename;
+			let notifyDates: Date[] = [];
+
+			let content = await vault.cachedRead(file);
+
+			let notifyDate: Date|null = null;
+			let pluginData = await this.loadData();
+
+			notifyDate = this.getDateFromFileProperties(content);
+			if (notifyDate)
+				notifyDates.push(notifyDate);
+
+			notifyDates.push(...this.getDatesFromFileBody(content));
+
+			if (notifyDates.length !== 0) {
+				let newNotifyDates: Date[] = [];
+
+				for (notifyDate of notifyDates) {
+					if (!this.isDateNotificationSent(fileName, notifyDate, pluginData))
+						newNotifyDates.push(notifyDate);
+				}
+
+				this.filesToNotify.push({fileName, notifyDates: newNotifyDates});
+			}
+		}
+
+		await this.sendNotifications();
+	}
+
+	private getDatesFromFileBody(content): Date[] {
+		let notifyDates: Date[] = [];
+		let lastOccurrence = -1;
+
+		while (true) {
+			lastOccurrence = content.indexOf(this.settings.fileSchedulePrefix, lastOccurrence + 1);
+			if (lastOccurrence == undefined || lastOccurrence === -1)
+				break;
+
+			let lastOccurrenceWithoutPrefix = lastOccurrence + this.settings.fileSchedulePrefix.length;
+			lastOccurrenceWithoutPrefix +=
+				content.slice(lastOccurrenceWithoutPrefix).length -
+				content.slice(lastOccurrenceWithoutPrefix).trimStart().length;
+
+			let spaceOccurrence = content.slice(lastOccurrenceWithoutPrefix).search(/[ \n]/)
+				+ lastOccurrenceWithoutPrefix;
+
+			if (spaceOccurrence === -1)
+				continue;
+
+			let notifyDateContent = content.substring(lastOccurrenceWithoutPrefix, spaceOccurrence).trim();
+
+			let date = this.getDateFromContent(notifyDateContent);
+			if (date)
+				notifyDates.push(date);
+		}
+
+		return notifyDates;
+	}
+
+	private async sendNotifications() {
+		for (const file of this.filesToNotify) {
+			for (let date of file.notifyDates) {
+				date = new Date(date.toDateString() + ` ${this.settings.notifyTime}`);
+				let now = new Date();
+
+				const diffTime = Math.abs(now - date);
+				const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+				if (date > now || diffDays > 5) {
+					continue;
+				}
+
+				let pluginData = await this.loadData();
+
+				if (pluginData[file.fileName] && pluginData[file.fileName][date.getTime()])
+					continue;
+
+				let message = `>📅 You have a task due on _${date.toDateString()}_\n👉 \`${file.fileName}\``;
+
+				this.sendMessageToBot(
+					this.settings.telegramToken,
+					this.settings.telegramChatId,
+					encodeURIComponent(message)
+				);
+
+				if (!pluginData[file.fileName])
+					pluginData[file.fileName] = {};
+
+				pluginData[file.fileName][date.getTime()] = true;
+
+				await this.saveData(pluginData);
+			}
+		}
+	}
+
+	private getDateFromContent(notifyDateContent: string): Date|null {
+		let notifyDate = new Date(notifyDateContent);
+
+		if (notifyDate.toString() === "Invalid Date" || isNaN(notifyDate.getDate()))
+			return null;
+
+		return notifyDate;
+	}
+
+	private isDateNotificationSent(fileName: string, notifyDate: Date, pluginData): boolean {
+		return pluginData[fileName] && pluginData[fileName][notifyDate.getTime()];
 	}
 
 	async loadSettings() {
@@ -89,21 +185,25 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	private sendMessageToBot(botToken: string, chatId: string, message: string): void {
+		fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=MarkdownV2`);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	private getDateFromFileProperties(content: string): Date|null {
+		if (content.substring(0, 3) !== '---')
+			return null;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		let propertiesContent = content.substring(3, content.indexOf('---', 2));
+		let scheduledProperty =
+			propertiesContent.split(`${this.settings.fileScheduleProperty}:`)[1] ?? null;
+
+		if (!scheduledProperty)
+			return null;
+
+		scheduledProperty = scheduledProperty.trim().split(/([ \n])/)[0];
+
+		return this.getDateFromContent(scheduledProperty);
 	}
 }
 
@@ -121,13 +221,57 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Telegram Bot Token')
+			.setDesc('')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your token')
+				.setValue(this.plugin.settings.telegramToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.telegramToken = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Telegram Chat ID')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('Enter your chat id')
+				.setValue(this.plugin.settings.telegramChatId)
+				.onChange(async (value) => {
+					this.plugin.settings.telegramChatId = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Schedule Property Name')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('Enter property name')
+				.setValue(this.plugin.settings.fileScheduleProperty)
+				.onChange(async (value) => {
+					this.plugin.settings.fileScheduleProperty = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Schedule Inline Prefix')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('Enter inline prefix')
+				.setValue(this.plugin.settings.fileSchedulePrefix)
+				.onChange(async (value) => {
+					this.plugin.settings.fileSchedulePrefix = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Notify Time')
+			.setDesc('Time for dates without time')
+			.addText(text => text
+				.setPlaceholder('Enter time')
+				.setValue(this.plugin.settings.notifyTime)
+				.onChange(async (value) => {
+					this.plugin.settings.notifyTime = value;
 					await this.plugin.saveSettings();
 				}));
 	}
